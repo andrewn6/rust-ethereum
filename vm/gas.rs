654cf21e7f79f775016c1fdb66ae63502a2318b3
Andrew <andrewnijmeh1@gmail.com>
2022-02-17 16:23:42 -0500

    add gas costs

---
+++ vm/gas.rs
@@ -0,0 +1,16 @@
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub struct Gas(pub u64);
+
+impl Gas {
+    pub const GasQuickStep: Gas = Gas(2);
+    pub const GasFatestStep: Gas = Gas(3);
+    pub const GasFastStep: Gas = Gas(5);
+    pub const GasMidStep: Gas = Gas(8);
+    pub const GasSlowStep: Gas = Gas(10);
+    pub const GasExtStep: Gas = Gas(20);
+
+}
+
+pub fn main() {
+}
+

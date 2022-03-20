3ce52002e26a3bda545c765a0ce37177ee801760
Andrew <andrewnijmeh1@gmail.com>
2022-03-20 14:30:10 -0400

    rm target

--- target/debug/build/crunchy-27a1376ebaa98c1c/build_script_build-27a1376ebaa98c1c.d
+++
@@ -1,5 +0,0 @@
-/Users/andrew/dev/rust-evm/target/debug/build/crunchy-27a1376ebaa98c1c/build_script_build-27a1376ebaa98c1c: /Users/andrew/.cargo/registry/src/github.com-1ecc6299db9ec823/crunchy-0.2.2/build.rs
-
-/Users/andrew/dev/rust-evm/target/debug/build/crunchy-27a1376ebaa98c1c/build_script_build-27a1376ebaa98c1c.d: /Users/andrew/.cargo/registry/src/github.com-1ecc6299db9ec823/crunchy-0.2.2/build.rs
-
-/Users/andrew/.cargo/registry/src/github.com-1ecc6299db9ec823/crunchy-0.2.2/build.rs:
--- target/debug/deps/hex-6e9741f5f5cf2b24.d
+++
@@ -1,8 +0,0 @@
-/Users/andrew/dev/rust-evm/target/debug/deps/hex-6e9741f5f5cf2b24.rmeta: /Users/andrew/.cargo/registry/src/github.com-1ecc6299db9ec823/hex-0.4.3/src/lib.rs /Users/andrew/.cargo/registry/src/github.com-1ecc6299db9ec823/hex-0.4.3/src/error.rs
-
-/Users/andrew/dev/rust-evm/target/debug/deps/libhex-6e9741f5f5cf2b24.rlib: /Users/andrew/.cargo/registry/src/github.com-1ecc6299db9ec823/hex-0.4.3/src/lib.rs /Users/andrew/.cargo/registry/src/github.com-1ecc6299db9ec823/hex-0.4.3/src/error.rs
-
-/Users/andrew/dev/rust-evm/target/debug/deps/hex-6e9741f5f5cf2b24.d: /Users/andrew/.cargo/registry/src/github.com-1ecc6299db9ec823/hex-0.4.3/src/lib.rs /Users/andrew/.cargo/registry/src/github.com-1ecc6299db9ec823/hex-0.4.3/src/error.rs
-
-/Users/andrew/.cargo/registry/src/github.com-1ecc6299db9ec823/hex-0.4.3/src/lib.rs:
-/Users/andrew/.cargo/registry/src/github.com-1ecc6299db9ec823/hex-0.4.3/src/error.rs:
